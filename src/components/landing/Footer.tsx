0e787e0bb7a1e74b186d3bf97544d4b1aa9ea25f
Dicky <dicky0290@gmail.com>
2026-02-09 19:53:13 +0700

    Remove accidental project files from root

--- src/components/landing/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="border-t border-zinc-200 py-8">
-            <div className="container mx-auto px-4 text-center text-zinc-600">
-                <p>&copy; 2026 FixIt. All rights reserved.</p>
-            </div>
-        </footer>
-    )
-}
--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./common";
-export * from "./user";
-export * from "./auth";
-export * from "./service";
-export * from "./order";
-export * from "./payment";
-export * from "./admin";
